81df56ddb354e3ef6f99719178a632f790c5e848
Tariq Ali <theeastrenman93@gmail.com>
2017-05-21 02:01:27 +0300

    Tariq Edit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+jQuery(document).ready(function($) {
+	sidebar();
+	function sidebar() {
+		'use strict';
 
+		var isOpen = false;
+		$('.__navbar .navbar-toggle').on('click' , function(e) {
+			if (!isOpen) {
+				$('.__navbar .nav-container').addClass('open');
+				$('body').append('<div class="sidebar_overlay"></div>');
+				isOpen = true;
+			} 
+
+			else {
+				$('.__navbar .nav-container').removeClass('open');
+				$('.sidebar_overlay').remove();
+				isOpen = false;
+			}
+		});
+	}
+});
